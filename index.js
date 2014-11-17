b6f5409c75a900938f84ab01afe09e1a0902aad1
Shruti Sharma <shrutijalewar@gmail.com>
2014-11-17 14:34:34 -0600

    final

--- index.js
+++ index.js
@@ -1,3 +1,86 @@
-/**
- * Created by Shruti on 11/17/14.
- */
+var port = process.env.PORT;
+var db = process.env.DB;
+var Hapi = require('hapi');
+var Joi = require('joi');
+var server = new Hapi.Server(port);
+
+var mongoose = require('mongoose');
+mongoose.connect(db);
+
+var Dog = mongoose.model('Dog', { name: String, age: Number});
+
+
+server.route({
+    config: {
+        description: 'this is the home page route',
+        notes: 'these are my routes',
+        tags: ['home', 'a', 'b']
+    },
+
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.route({
+    method: 'POST',
+    path: '/dogs',
+    handler: function (request, reply) {
+        var puppy = new Dog (request.payload);
+        puppy.save(function(){
+            reply(puppy);
+        });
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + request.params.name + '!' + request.query.limit);
+    },
+    config: {
+        validate: {
+            params: {
+                name: Joi.string().min(3).max(10)
+            },
+            query: {
+                limit: Joi.number().required().min(10)
+            }
+        }
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/static/{param*}',
+    handler: {
+        directory: {
+            path: 'static'
+        }
+    }
+});
+
+server.pack.register(
+    [
+        {
+            plugin: require('good'),
+            options: {
+                reporters: [{
+                    reporter: require('good-console'),
+                    args: [{log: '*', request: '*'}]
+                }]
+            }
+        },
+        {plugin: require('lout')}
+
+], function (err) {
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+    server.start(function () {
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
+});
